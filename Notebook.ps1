1cdc32e0eec17319d86c07f208a748c0945758e7
Tobias Schlienger <tobias.schlienger@gmail.com>
2017-05-11 10:17:05 +0200

    Add files via upload

---
+++ Notebook.ps1
@@ -0,0 +1,5 @@
+﻿$serial = (Get-WmiObject -Class Win32_BIOS).SerialNumber
+$os = (Get-ComputerInfo).WindowsProductName -replace "\s", "%20"
+Start "http://localhost/notebook/register?serial=$serial&os=$os"
+Rename-Computer -NewName (Invoke-WebRequest -Uri "http://localhost/notebook/hostname.txt").Content
+Restart-Computer
